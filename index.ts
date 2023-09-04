9190cdf7997ce8fdf857b73da9f59a81cc9ffd40
dIvashinin <dmitry.ivashinin@gmail.com>
2023-09-04 16:39:25 +0200

    followed TS spike

--- index.ts
+++ index.ts
@@ -2,3 +2,32 @@ console.log("Hello World");
 async function helloWorld() {
     return "hello";
 }
+
+type Mentor = "Raul" | "Lucas"
+interface Person {
+    name: string;
+    age: number;
+    mentor?: Mentor;
+    pet?: boolean | string;
+}
+
+const lucas: Person = {
+    name: "Lucas",
+    age: 23,
+    mentor: "Lucas",
+    pet: true,
+};
+
+type People = Person[]
+
+const people : People = [{age:23, name: "Karl"}, lucas]
+people.forEach(person =>{
+    const plus3=person.age+3;
+    console.log('plus3 :>> ', plus3);
+});
+
+function multiply (x,y) {
+    return x*y;
+}
+const res = multiply("one","two");
+console.log('res :>> ', res);
