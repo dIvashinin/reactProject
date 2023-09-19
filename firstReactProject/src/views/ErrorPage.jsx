a224100f6e39554c64d06010563847d9b5a9894f
dIvashinin <dmitry.ivashinin@gmail.com>
2023-09-19 10:02:32 +0200

    added go back button in error page

--- firstReactProject/src/views/ErrorPage.jsx
+++ firstReactProject/src/views/ErrorPage.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import {useRouteError} from "react-router-dom";
+import {useNavigate, useRouteError} from "react-router-dom";
 
 function ErrorPage() {
     const error = useRouteError();
     console.log('error :>> ', error);
+
+  // useNavigate hook
+
+  const navigateTo = useNavigate()
+  console.log('navigateTo :>> ', navigateTo);
+  const goHome = () => {
+    navigateTo("/");
+  };
+
   return (
     <>
     <h2>Looks like you you're not supposed to be here. Pls check what you have typed</h2>
     <h3>{error.error.message}</h3>
+    <button onClick={goHome}>go back</button>
     
     </>
   );
