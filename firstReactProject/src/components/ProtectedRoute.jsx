fb21c7934a43e535f17c41ac69a792ad76012111
dIvashinin <dmitry.ivashinin@gmail.com>
2023-09-25 10:53:41 +0200

    protected route is now working in 'about' when user is logged in/ out

--- firstReactProject/src/components/ProtectedRoute.jsx
+++ firstReactProject/src/components/ProtectedRoute.jsx
@@ -1,10 +1,14 @@
 import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 function ProtectedRoute(props) {
+    const {user} = useContext(AuthContext);
+    console.log('props :>> ', props);
   // this bouncer will check you and let you in or not
   return (
     <div>
-      <h2>sorry but you are not allowed here</h2>
+      
+      {user ? props.children : <h2>sorry but you are not allowed here</h2>}
     </div>
   );
 }
