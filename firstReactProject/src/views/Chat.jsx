898f8e272d4e9cda8d35d73662d040de5a21b52b
dIvashinin <dmitry.ivashinin@gmail.com>
2023-09-28 15:57:36 +0200

    now i have access to my messagesArray

--- firstReactProject/src/views/Chat.jsx
+++ firstReactProject/src/views/Chat.jsx
@@ -1,27 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from '../config/firebaseConfig';
+import { db } from "../config/firebaseConfig";
 
 function Chat() {
+  //2.Store messages in a variable (state variable)
+  const [chatMessages, setChatMessages] = useState({});
 
+  //1.Get documents from our Database
+  const getMessages = async () => {
+    const querySnapshot = await getDocs(collection(db, "chat"));
+    const messagesArray = [];
+    querySnapshot.forEach((doc) => {
+      console.log("doc :>> ", doc.data());
+      console.log(`${doc.id} => ${doc.data()}`);
+      messagesArray.push(doc.data());
+    });
+    console.log('messagesArray :>> ', messagesArray);
+  };
 
-    const getMessages = async () => {
-        const querySnapshot = await getDocs(collection(db, "chat"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-});
-    };
-
-useEffect(() => {
-  getMessages();
-}, []);
-
+  useEffect(() => {
+    getMessages();
+  }, []);
 
   return (
     <div>
-        <h1>Chat</h1>
+      <h1>Chat</h1>
+      {/* Display messages here */}
     </div>
   );
 }
 
-export default Chat
+export default Chat;
