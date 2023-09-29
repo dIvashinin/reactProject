8b115d6dcf49f886207ede5116b0d47b371449bb
dIvashinin <dmitry.ivashinin@gmail.com>
2023-09-29 16:30:21 +0200

    added live update of messages in chat

--- firstReactProject/src/views/Chat.jsx
+++ firstReactProject/src/views/Chat.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { collection, getDocs, addDoc, query, where, onSnapshot } from "firebase/firestore";
 import { db } from "../config/firebaseConfig";
+import { AuthContext } from "../context/AuthContext";
 
 function Chat() {
+  // import (subscribe) user from the context
+  const {user} = useContext(AuthContext);
+
   //2.Store messages in a variable (state variable)
   const [chatMessages, setChatMessages] = useState([]);
 
@@ -36,12 +40,37 @@ function Chat() {
     return formattedDate;
   };
 
-  const submitMessage = () => {
+    //7. Create a function to send messages to DB
+  const submitMessage = async () => {
     console.log('textMsg :>> ', textMsg);
+    const newChatMsg = {
+        // if the user exists, then
+        author: user?.email,
+        text: textMsg,
+        date: new Date()
+    };
+    console.log('newChatMsg :>> ', newChatMsg);
+    // store the newChatMsg in my Database
+    const docRef = await addDoc(collection(db, "chat"), newChatMsg);
+      console.log("Document written with ID: ", docRef.id);
+  };
+
+//   get messages with live update
+  const getMessagesLiveUpdate = () => {
+    const q = query(collection(db, "chat"));
+const unsubscribe = onSnapshot(q, (querySnapshot) => {
+  const messagesArray = [];
+  querySnapshot.forEach((doc) => {
+    messagesArray.push(doc.data());
+  });
+  console.log('messagesArray :>> ', messagesArray);
+  setChatMessages(messagesArray);
+});
   }
 
   useEffect(() => {
     getMessages();
+    getMessagesLiveUpdate();
   }, []);
 
   return (
